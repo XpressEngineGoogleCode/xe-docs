405ca639016939f778a50dec320bbe816ebb5d6d
xe_dev <xe_dev@7d239364-9d55-e763-a29c-5cb89c5a5101>
2011-07-15 13:11:22 +0000

    fix , reply to problem when adding comment.

--- xedocs/skins/xe_xedocs/js/manual.js
+++ xedocs/skins/xe_xedocs/js/manual.js
@@ -18,7 +18,7 @@ function completeInsertComment(ret_obj) {
     var comment_srl = ret_obj['comment_srl'];
 
     var url = current_url.setQuery('mid',mid).setQuery('document_srl',document_srl).setQuery('act','dispXedocsCommentEditor');
-    if(comment_srl) url = url.setQuery('rnd',comment_srl)+"#comment_"+comment_srl;
+    if(comment_srl) url = url.setQuery('rnd',comment_srl)+"comment_"+comment_srl;
 
     location.href = url;
 }
