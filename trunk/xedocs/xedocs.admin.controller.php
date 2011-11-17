a57f2b85e9fe4d207bbb12e78bb2dc7749885ff1
corina.udrescu@gmail.com <corina.udrescu@gmail.com@7d239364-9d55-e763-a29c-5cb89c5a5101>
2011-11-17 13:19:00 +0000

    Cleaned up admin controller - all but keywords.

--- trunk/xedocs/xedocs.admin.controller.php
+++ trunk/xedocs/xedocs.admin.controller.php
@@ -1,183 +1,182 @@
 <?php
 
-class xedocsAdminController extends xedocs {
+    /**
+     * @class  xedocsAdminController
+     *
+     * Contains backend views' action methods
+     **/
 
-	function init() {
-		$this->setTemplatePath($this->module_path.'tpl');
-	}
+    class xedocsAdminController extends xedocs {
 
-	function is_good_archive_url($url){
-		return true;
-	}
+        /**
+         * @brief Action method setup - executed before every proc
+         */
+        function init() {
+            $this->setTemplatePath($this->module_path.'tpl');
+        }
 
-	function procXedocsAdminInsertManual(){
+        /**
+         * @brief Adds a new manual
+         */
+        function procXedocsAdminInsertManual(){
+            $oModuleController = &getController('module');
+            $oModuleModel = &getModel('module');
 
-		debug_syslog(1, "procXedocsAdminInsertManual\n");
+            $toc_location = Context::get('toc_location');
+            if(!isset($toc_location)){
+                Context::set('toc_location', "Left"); //set default
+            }
 
-		$oModuleController = &getController('module');
-		$oModuleModel = &getModel('module');
+            $args = Context::getRequestVars();
+            $args->module = 'xedocs';
+            $args->mid = $args->manual_name;
+            if($args->use_comment!='N'){
+                    $args->use_comment = 'Y';
+            }
 
-		$toc_location = Context::get('toc_location');
-		if( !isset($toc_location) ){
-			Context::set('toc_location', "Left"); //set default
-		}
+            unset($args->manual_name);
 
-		$args = Context::getRequestVars();
-		debug_syslog(1, "procXedocsAdminInsertManual args: ".print_r($args, true)."\n");
-		$args->module = 'xedocs';
-		$args->mid = $args->manual_name;
-		if($args->use_comment!='N'){
-			$args->use_comment = 'Y';
-		}
+            if($args->module_srl) {
+                    $module_info = $oModuleModel->getModuleInfoByModuleSrl($args->module_srl);
+                    if($module_info->module_srl != $args->module_srl){
+                            unset($args->module_srl);
+                    }
+            }
 
+            if(!$args->module_srl) {
+                    $output = $oModuleController->insertModule($args);
+                    $msg_code = 'success_registed';
+            } else {
+                    $output = $oModuleController->updateModule($args);
+                    $msg_code = 'success_updated';
+            }
 
-		debug_syslog(1, "args :\n ".print_r($args, true)."\n");
-		unset($args->manual_name);
+            if(!$output->toBool()) {
+                    return $output;
+            }
 
-		if($args->module_srl) {
-			$module_info = $oModuleModel->getModuleInfoByModuleSrl($args->module_srl);
-			if($module_info->module_srl != $args->module_srl){
-				unset($args->module_srl);
-			}
-		}
-
-		if(!$args->module_srl) {
-			$output = $oModuleController->insertModule($args);
-			$msg_code = 'success_registed';
-		} else {
-			$output = $oModuleController->updateModule($args);
-			$msg_code = 'success_updated';
-		}
-
-		if(!$output->toBool()) {
-			return $output;
-		}
-
-		$this->add('page',Context::get('page'));
-		$this->add('module_srl',$output->get('module_srl'));
-		$this->setMessage($msg_code);
-
-		debug_syslog(1, "Inserted manual module ". $module_name);
-
-	}
+            $this->add('page',Context::get('page'));
+            $this->add('module_srl',$output->get('module_srl'));
+            $this->setMessage($msg_code);
+        }
 
         /**
          * @brief Recreates document aliases
          */
         function procXedocsAdminArrangeList() {
-                $oModuleModel = &getModel('module');
-                $oDocumentController = &getController('document');
+            $oModuleModel = &getModel('module');
+            $oDocumentController = &getController('document');
 
-                $module_srl = Context::get('module_srl');
-                $module_info = $oModuleModel->getModuleInfoByModuleSrl($module_srl);
-                if(!$module_info->module_srl || $module_info->module != 'xedocs') return new Object(-1,'msg_invalid_request');
+            $module_srl = Context::get('module_srl');
+            $module_info = $oModuleModel->getModuleInfoByModuleSrl($module_srl);
+            if(!$module_info->module_srl || $module_info->module != 'xedocs') return new Object(-1,'msg_invalid_request');
 
-                $args->module_srl = $module_srl;
-                $output = executeQueryArray('xedocs.getDocumentWithoutAlias', $args);
-                if(!$output->toBool() || !$output->data) return new Object();
+            $args->module_srl = $module_srl;
+            $output = executeQueryArray('xedocs.getDocumentWithoutAlias', $args);
+            if(!$output->toBool() || !$output->data) return new Object();
 
-                foreach($output->data as $key => $val) {
-                        if($val->alias_srl) continue;
-                        $result = $oDocumentController->insertAlias($module_srl, $val->document_srl, $val->alias_title);
-                        if(!$result->toBool()) $oDocumentController->insertAlias($module_srl, $val->document_srl, $val->alias_title.'_'.$val->document_srl);
-                }
+            foreach($output->data as $key => $val) {
+                    if($val->alias_srl) continue;
+                    $result = $oDocumentController->insertAlias($module_srl, $val->document_srl, $val->alias_title);
+                    if(!$result->toBool()) $oDocumentController->insertAlias($module_srl, $val->document_srl, $val->alias_title.'_'.$val->document_srl);
+            }
+        }
+
+        /**
+         * @brief Deletes a manual
+         */
+        function procXedocsAdminDelete()
+        {
+            $module_srl = Context::get('module_srl');
+
+            $oModuleController = &getController('module');
+            $output = $oModuleController->deleteModule($module_srl);
+
+            if(!$output->toBool()){
+                    return $output;
+            }
+
+            $this->add('module','xedocs');
+            $this->add('page',Context::get('page'));
+            $this->setMessage('success_deleted');
         }
 
 
-	function procXedocsAdminDelete()
-	{
-		debug_syslog(1, "procXedocsAdminDelete\n");
+        function _update_keyword($keyword, $orig_keyword=null)
+        {
 
-		$module_srl = Context::get('module_srl');
+                debug_syslog(1, "_update_keyword(".$keyword.", orig=".$orig_keyword.")\n");
+                $module_srl = Context::get('module_srl');
+                $target_document_srl = Context::get('target_document_srl');
 
-		$oModuleController = &getController('module');
-		$output = $oModuleController->deleteModule($module_srl);
+                $oXedocsModel = &getModel('xedocs');
+                $updated = $oXedocsModel->update_keyword($module_srl, $orig_keyword, $keyword, $target_document_srl);
+                if($updated){
+                        debug_syslog(1, "keyword updated\n");
+                }
 
-		if(!$output->toBool()){
-			return $output;
-		}
+        }
 
-		$this->add('module','xedocs');
-		$this->add('page',Context::get('page'));
-		$this->setMessage('success_deleted');
-	}
+        function procXedocsAdminEditKeyword()
+        {
+                debug_syslog(1, "procXedocsAdminEditKeyword\n");
+                $orig_keyword = Context::get('orig_title');
+                $keyword = Context::get('title');
 
+                $this->_update_keyword($keyword, $orig_keyword);
+                $this->setMessage("success");
+                debug_syslog(1, "procXedocsAdminEditKeyword complete\n");
+        }
 
-	function _update_keyword($keyword, $orig_keyword=null)
-	{
+        function procXedocsAdminAddKeyword()
+        {
+                debug_syslog(1, "procXedocsAdminAddKeyword\n");
 
-		debug_syslog(1, "_update_keyword(".$keyword.", orig=".$orig_keyword.")\n");
-		$module_srl = Context::get('module_srl');
-		$target_document_srl = Context::get('target_document_srl');
+                $keyword = Context::get('title');
+                $orig_keyword = null;
 
-		$oXedocsModel = &getModel('xedocs');
-		$updated = $oXedocsModel->update_keyword($module_srl, $orig_keyword, $keyword, $target_document_srl);
-		if($updated){
-			debug_syslog(1, "keyword updated\n");
-		}
-
-	}
-
-	function procXedocsAdminEditKeyword()
-	{
-		debug_syslog(1, "procXedocsAdminEditKeyword\n");
-		$orig_keyword = Context::get('orig_title');
-		$keyword = Context::get('title');
-
-		$this->_update_keyword($keyword, $orig_keyword);
-		$this->setMessage("success");
-		debug_syslog(1, "procXedocsAdminEditKeyword complete\n");
-	}
-
-	function procXedocsAdminAddKeyword()
-	{
-		debug_syslog(1, "procXedocsAdminAddKeyword\n");
-
-		$keyword = Context::get('title');
-		$orig_keyword = null;
-
-		$this->_update_keyword($keyword, $orig_keyword);
-		$this->setMessage("success");
-		debug_syslog(1, "procXedocsAdminAddKeyword complete\n");
-	}
+                $this->_update_keyword($keyword, $orig_keyword);
+                $this->setMessage("success");
+                debug_syslog(1, "procXedocsAdminAddKeyword complete\n");
+        }
 
 
-	function procXedocsAdminCompileKeywords(){
+        function procXedocsAdminCompileKeywords(){
 
-		debug_syslog(1, "procXedocsAdminCompileKeywords\n");
-		set_time_limit(0);
+                debug_syslog(1, "procXedocsAdminCompileKeywords\n");
+                set_time_limit(0);
 
-		$oXedocsModel = &getModel('xedocs');
+                $oXedocsModel = &getModel('xedocs');
 
-		$help_name = Context::get('help_name');
-		$module_srl = Context::get('module_srl');
+                $help_name = Context::get('help_name');
+                $module_srl = Context::get('module_srl');
 
-		debug_syslog(1, "module_srl='".$module_srl."'\n");
-		debug_syslog(1, "help_name='".$help_name."'\n");
-
-
-		$docs = $oXedocsModel->getDocumentList($module_srl);
-
-		debug_syslog(1, "there are ".count($docs)." documents \n getting keywords ...\n");
+                debug_syslog(1, "module_srl='".$module_srl."'\n");
+                debug_syslog(1, "help_name='".$help_name."'\n");
 
 
-		$keywords = $oXedocsModel->getKeywordTargets($docs, 10000);
+                $docs = $oXedocsModel->getDocumentList($module_srl);
 
-		debug_syslog(1, "extract keywords complete count = ".count($keywords)."\n");
+                debug_syslog(1, "there are ".count($docs)." documents \n getting keywords ...\n");
 
-		$oModuleModel = &getModel('module');
 
-		$extra_vars = $oModuleModel->getModuleExtraVars($module_srl);
-		$update_args = $extra_vars[$module_srl];
-		$update_args->{'keywords'} = $oXedocsModel->keyword_list_to_string($keywords);
-		Context::set('filter_keyword', null);
+                $keywords = $oXedocsModel->getKeywordTargets($docs, 10000);
 
-		$oModuleController = &getController('module');
-		$oModuleController->insertModuleExtraVars($module_srl, $update_args);
+                debug_syslog(1, "extract keywords complete count = ".count($keywords)."\n");
 
-		debug_syslog(1, "compile keywords complete");
-		$this->setMessage('success_compiled');
-	}
+                $oModuleModel = &getModel('module');
 
-}
+                $extra_vars = $oModuleModel->getModuleExtraVars($module_srl);
+                $update_args = $extra_vars[$module_srl];
+                $update_args->{'keywords'} = $oXedocsModel->keyword_list_to_string($keywords);
+                Context::set('filter_keyword', null);
+
+                $oModuleController = &getController('module');
+                $oModuleController->insertModuleExtraVars($module_srl, $update_args);
+
+                debug_syslog(1, "compile keywords complete");
+                $this->setMessage('success_compiled');
+        }
+
+    }
 ?>
